c3bef381d18ed351b792015239ea9077fe23dd5e
Phulsing <phulsing@deccansoft.net>
2025-09-21 16:11:24 +0530

    fhvy

---
+++ src/components/dashboard/setting.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Setings() {
+    return (
+        <div className="flex justify-center items-center h-full w-full flex-col gap-6">
+           Settings
+        </div>
+    )
+}
